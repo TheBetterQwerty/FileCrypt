8ba5dcf44e7776db4bfa1d5d40b10127b58c57fd
qwerty <sepiolsam2023@gmail.com>
2026-07-23 22:20:00 +0530

    Finished chunk based file encryptor TODO: Add mkdir -p feature

--- src/main.c
+++ src/main.c
@@ -11,12 +11,15 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 
-#define MAX_PATH 2096
-#define AES_ENC 256
-#define IV_SIZE 16
+#define MAX_PATH	2096
+#define AES_ENC		256
+#define IV_SIZE		16
+#define CHUNK_SIZE	64 * 1024
 
 unsigned char magic_cookie[] = {0xDE, 0xAD, 0xBE, 0xEF}; // 0xDEADBEEF
 size_t magic_cookie_sz = sizeof(magic_cookie) / sizeof(magic_cookie[0]);
+char* output_path = NULL;
+char* input_path = NULL;
 
 void handle_errors(const char *msg) {
 	fprintf(stderr, "[!] Error: %s\n", msg);
@@ -79,9 +82,133 @@ int get_key(const char* buf, char* key) {
 	return 0;
 }
 
-void encrypt_file(const char* filename, const unsigned char* key) {
-	FILE* fptr = fopen(filename, "rb+");
+int serialize_path(const char* current_file, char* output_file) {
+	int input_dir_size = strlen(input_path);
+	int output_file_size = strlen(output_path);
+
+	if (strncmp(current_file, input_path, input_dir_size) != 0) {
+		return 1;
+	}
+
+	const char* relative = current_file + input_dir_size;
+	int written = snprintf(output_file, MAX_PATH, "%s%s", output_path, relative);
+
+	return written >= 0 && written < output_file_size; // 0
+}
+
+void decrypt_file(const char* filename, const char* outfile, const unsigned char* key) {
+	FILE* fptr = fopen(filename, "rb");
 	if (!fptr) {
+		fprintf(stderr, "[!] Error: %s\n", strerror(errno));
+		return;
+	}
+
+	fseek(fptr, 0, SEEK_END);
+	size_t size = ftell(fptr) * sizeof(char);
+	fseek(fptr, 0, SEEK_SET);
+
+	if (size == 0) {
+		fclose(fptr);
+		return;
+	}
+
+	char mgc_cookie[magic_cookie_sz];
+
+	if (magic_cookie_sz != fread(mgc_cookie, 1, magic_cookie_sz, fptr)) {
+		fprintf(stderr, "[!] Error: Reading from headers %s file\n", filename);
+		fclose(fptr);
+		return;
+	}
+
+	if (0 != memcmp(mgc_cookie, magic_cookie, magic_cookie_sz)) {
+		fprintf(stdout, "Skipping %s as it isn't encrypted\n", filename);
+		fclose(fptr);
+		return;
+	}
+
+	unsigned char iv[IV_SIZE];
+
+	if (IV_SIZE != fread(iv, 1, IV_SIZE, fptr)) {
+		fprintf(stderr, "[!] Error: Reading from nonce %s file\n", filename);
+		fclose(fptr);
+		return;
+	}
+
+	FILE* out_ptr = fopen(outfile, "wb");
+	if (!out_ptr) {
+		fprintf(stderr, "[!] %s\n", strerror(errno));
+		fclose(fptr);
+		return;
+	}
+
+	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+	if (!ctx) {
+		fclose(fptr);
+		fclose(out_ptr);
+		handle_errors("Evp_cipher_ctx_new()");
+	}
+
+	int len = 0, plaintext_len = 0;
+
+	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {
+		fclose(fptr);
+		fclose(out_ptr);
+		handle_errors("DecryptInit");
+	}
+
+	unsigned char buffer[CHUNK_SIZE];
+	unsigned char plaintext[CHUNK_SIZE];
+	ssize_t nbytes = 0;
+
+	do {
+		nbytes = fread(buffer, 1, size, fptr);
+
+		if (nbytes < 0) {
+			fprintf(stderr, "[!] Error: Reading file %s\n", filename);
+			return;
+		}
+
+		if (nbytes == 0) return;
+
+		if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, buffer, nbytes)) {
+			fclose(fptr);
+			fclose(out_ptr);
+			handle_errors("EVP_DecryptUpdate");
+		}
+
+		plaintext_len = len;
+
+		if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) {
+			fclose(fptr);
+			fclose(out_ptr);
+			handle_errors("EVP_DecryptFinal_ex");
+		}
+
+		plaintext_len += len;
+
+		if ((unsigned long) plaintext_len != fwrite(plaintext, 1, plaintext_len, out_ptr)) {
+			fclose(out_ptr);
+			fclose(fptr);
+			handle_errors("fwrite");
+		}
+	} while (nbytes > 0);
+
+	fclose(out_ptr);
+	fclose(fptr);
+	EVP_CIPHER_CTX_free(ctx);
+	printf("[+] File %s was decrypted as stored at %s\n", filename, outfile);
+}
+
+void encrypt_file(const char* filename, const char* outfile, const unsigned char* key) {
+	FILE* fptr = fopen(filename, "rb");
+	if (!fptr) {
+		fprintf(stderr, "[!] Error: %s\n", strerror(errno));
+		return;
+	}
+
+	// Join outfile/filename smth like that
+	FILE* out_ptr = fopen(outfile, "wb");
+	if (!out_ptr) {
 		fprintf(stderr, "[!] %s\n", strerror(errno));
 		return;
 	}
@@ -95,206 +222,70 @@ void encrypt_file(const char* filename, const unsigned char* key) {
 		return;
 	}
 
-	char* buffer = (char*) malloc(size);
-	if (!buffer) {
-		fprintf(stderr, "[!] Error allocating memory!\n");
-		fclose(fptr);
-		return;
-	}
-
-	if (size != fread(buffer, 1, size, fptr)) {
-		fprintf(stderr, "[!] Error reading from file\n");
-		fclose(fptr);
-		free(buffer);
-		return;
-	}
-
-	unsigned char* ciphertext = malloc(size + IV_SIZE);
-	if (!ciphertext) {
-		fprintf(stderr, "[!] Error allocating memory!\n");
-		fclose(fptr);
-		free(buffer);
-		return;
-	}
+	unsigned char buffer[CHUNK_SIZE];
+	unsigned char ciphertext[CHUNK_SIZE + IV_SIZE];
+	ssize_t nbytes;
 
 	unsigned char iv[IV_SIZE];
 	if (!RAND_bytes(iv, IV_SIZE)) handle_errors("Randbyte failed");
 
-	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
     if (!ctx) handle_errors("EVP_CIPHER_CTX_new");
 
-    int len = 0, ciphertext_len = 0;
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+	int len = 0, ciphertext_len = 0;
+	if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
         handle_errors("EVP_EncryptInit_ex");
 
-    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, (unsigned char*) buffer, (int) size))
-        handle_errors("EVP_EncryptUpdate");
-
-    ciphertext_len = len;
-
-    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-        handle_errors("EVP_EncryptFinal_ex");
-
-    ciphertext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-
-	fseek(fptr, 0, SEEK_SET);
-	if (magic_cookie_sz != fwrite(magic_cookie, 1, magic_cookie_sz, fptr)) {
+	if (magic_cookie_sz != fwrite(magic_cookie, 1, magic_cookie_sz, out_ptr)) {
 		fprintf(stderr, "[!] Error writting magic cookie to file!\n");
 		fclose(fptr);
-		free(ciphertext);
-		free(buffer);
+		fclose(out_ptr);
 		return;
 	}
 
-	if (IV_SIZE != fwrite(iv, 1, IV_SIZE, fptr)) {
+	if (IV_SIZE != fwrite(iv, 1, IV_SIZE, out_ptr)) {
 		fprintf(stderr, "[!] Error writting iv to file!\n");
 		fclose(fptr);
-		free(ciphertext);
-		free(buffer);
+		fclose(out_ptr);
 		return;
 	}
 
-	if (ciphertext_len != (int) fwrite(ciphertext, 1, ciphertext_len, fptr)) {
-		fprintf(stderr, "[!] Error writting encrypted to file!\n");
-		fclose(fptr);
-		free(ciphertext);
-		free(buffer);
-		return;
-	}
+	do {
+		nbytes = fread(buffer, 1, size, fptr);
 
-	if (0 != ftruncate(fileno(fptr), IV_SIZE + ciphertext_len)) {
-        fprintf(stderr, "[!] Failed to truncate file\n");
-		fclose(fptr);
-		free(ciphertext);
-		free(buffer);
-		return;
-	}
+		if (nbytes < 0) {
+			fprintf(stderr, "[!] Error: Reading file %s\n", filename);
+			return;
+		}
 
-	// cleanup
-	fclose(fptr);
-	free(ciphertext);
-	free(buffer);
+		if (nbytes == 0) return;
 
-	printf("[+] Encrypted %s\n", filename);
-}
+		if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, buffer, (int) nbytes))
+			handle_errors("EVP_EncryptUpdate");
 
-void decrypt_file(const char* filename, const unsigned char* key) {
-	FILE* fptr = fopen(filename, "rb+");
-	if (!fptr) {
-		fprintf(stderr, "[!] %s\n", strerror(errno));
-		return;
-	}
+		ciphertext_len = len;
 
-	fseek(fptr, 0, SEEK_END); // go to end of file
-	size_t file_size = ftell(fptr) * sizeof(char);
-	fseek(fptr, 0, SEEK_SET); // go to start of file
+		if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+			handle_errors("EVP_EncryptFinal_ex");
 
-	if (file_size == 0) {
-		fclose(fptr);
-		return;
-	}
+		ciphertext_len += len;
 
-	char* buffer = (char*) malloc(file_size);
-	if (!buffer) {
-		fprintf(stderr, "[!] Error allocating memory!\n");
-		fclose(fptr);
-		return;
-	}
+		if (ciphertext_len != (int) fwrite(ciphertext, 1, ciphertext_len, out_ptr)) {
+			fprintf(stderr, "[!] Error writting encrypted to file!\n");
+			fclose(fptr);
+			fclose(out_ptr);
+			return;
+		}
 
-	if (file_size != fread(buffer, 1, file_size, fptr)) {
-		fprintf(stderr, "[!] Error reading from file\n");
-		fclose(fptr);
-		free(buffer);
-		return;
-	}
-
-	if (0 != memcmp(buffer, magic_cookie, magic_cookie_sz)) {
-		fprintf(stdout, "Skipping %s as it isn't encrypted\n", filename);
-		fclose(fptr);
-		free(buffer);
-		return;
-	}
-
-	unsigned char iv[IV_SIZE];
-	memcpy(iv, buffer + magic_cookie_sz, IV_SIZE);
-
-	char* buffer_offset = buffer + magic_cookie_sz + IV_SIZE; // move pointer to start of the message
-
-	unsigned char* out = (unsigned char*) malloc(file_size);
-	if (!out) {
-		free(buffer);
-		fclose(fptr);
-		fprintf(stderr, "[!] Error allocating memory!\n");
-		return;
-	}
-
-	// Decryption Starts
-	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-	if (!ctx) {
-		free(out);
-		free(buffer);
-		fclose(fptr);
-		handle_errors("Evp_cipher_ctx_new()");
-	}
-
-	int len = 0, plaintext_len = 0;
-
-	if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) {
-		free(out);
-		free(buffer);
-		fclose(fptr);
-		handle_errors("DecryptInit");
-	}
-
-	if (1 != EVP_DecryptUpdate(
-		ctx, out, &len,
-		(const unsigned char*) buffer_offset,
-		(file_size - IV_SIZE - magic_cookie_sz))
-	) {
-		free(buffer);
-		free(out);
-		fclose(fptr);
-		handle_errors("EVP_DecryptUpdate");
-	}
-
-	plaintext_len = len;
-
-	if (1 != EVP_DecryptFinal_ex(ctx, out + len, &len)) {
-		free(buffer);
-		free(out);
-		fclose(fptr);
-		printf("[%d] %s\n", __LINE__, strerror(errno));
-		handle_errors("EVP_DecryptFinal_ex");
-	}
-
-	plaintext_len += len;
-
-	fseek(fptr, 0, SEEK_SET);
-	if ((unsigned long) plaintext_len != fwrite(out, 1, plaintext_len, fptr)) {
-		free(buffer);
-		free(out);
-		fclose(fptr);
-		handle_errors("fwrite");
-	}
-
-	if (0 != ftruncate(fileno(fptr), plaintext_len)) {
-		free(buffer);
-		free(out);
-		fclose(fptr);
-		handle_errors("ftruncate");
-	}
+	} while (nbytes > 0);
 
 	EVP_CIPHER_CTX_free(ctx);
-	free(buffer);
-	free(out);
 	fclose(fptr);
-
-	printf("[+] Decrypted %s\n", filename);
+	fclose(out_ptr);
+	printf("[+] File %s was encrypted as stored at %s\n", filename, outfile);
 }
 
-void iter_folder(const char* path, const char* key, int enc) {
+void iter_folder(const char* path, const char* outpath, const unsigned char* key, int enc) {
 	struct stat path_stat;
 	if (0 != lstat(path, &path_stat)) {
 		fprintf(stderr, "[!] %s\n", strerror(errno));
@@ -302,7 +293,7 @@ void iter_folder(const char* path, const char* key, int enc) {
 	}
 
 	if (S_ISREG(path_stat.st_mode)) {
-		enc ? encrypt_file(path, (const unsigned char*) key) : decrypt_file(path, (const unsigned char*) key);
+		enc ? encrypt_file(path, outpath, key) : decrypt_file(path, outpath, key);
 		return;
 	}
 
@@ -333,11 +324,13 @@ void iter_folder(const char* path, const char* key, int enc) {
 		}
 
 		if (S_ISDIR(_path_stat.st_mode)) {
-			iter_folder(file, key, enc);
+			iter_folder(file, outpath, key, enc);
 		}
 
 		if (S_ISREG(_path_stat.st_mode)) {
-			enc ? encrypt_file(file, (const unsigned char*) key) : decrypt_file(file, (const unsigned char*) key);
+			char output_file[MAX_PATH];
+			if (serialize_path(file, output_file)) continue;
+			enc ? encrypt_file(file, output_file, key) : decrypt_file(file, output_file, key);
 		}
 	}
 
@@ -362,8 +355,8 @@ int main(int args, char** argv) {
 		return 1;
 	}
 
-	char key[SHA256_DIGEST_LENGTH], _key[SHA256_DIGEST_LENGTH];
-	int flag = 0;
+	unsigned char key[SHA256_DIGEST_LENGTH], _key[SHA256_DIGEST_LENGTH];
+	int flag = -1;
 
 	for (int i = 1; i < args; i++) {
 		if ((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "-h") == 0)) {
@@ -372,18 +365,18 @@ int main(int args, char** argv) {
 		}
 
 		if ((strcmp(argv[i], "--encrypt") == 0) || (strcmp(argv[i], "-e") == 0)) {
-			flag++;
+			flag = 1;
 
 			if ( i + 1 >= args) {
 				printf("[?] Missing argument for %s!\n", argv[i]);
 				break;
 			}
 
-			if (get_key("[+] Enter master key ->", key)) {
+			if (get_key("[+] Enter master key: ", (char*) key)) {
 				return 1;
 			}
 
-			if (get_key("[+] Enter master key again ->", _key)) {
+			if (get_key("[+] Enter master key again: ", (char*) _key)) {
 				return 1;
 			}
 
@@ -392,33 +385,58 @@ int main(int args, char** argv) {
 				return 1;
 			}
 
-			iter_folder(argv[i + 1], (const char*) key, 1);
-			break;
+			input_path = argv[i + 1];
+			continue;
 		}
 
 		if ((strcmp(argv[i], "--decrypt") == 0) || (strcmp(argv[i], "-d") == 0)) {
-			flag++;
+			flag = 0;
 
 			if ( i + 1 >= args) {
 				printf("[?] Missing argument for %s!\n", argv[i]);
 				break;
 			}
 
-			if (get_key("[+] Enter master key ->", key)) {
+			if (get_key("[+] Enter master key ->", (char*) key)) {
 				return 1;
 			}
 
-			iter_folder(argv[i + 1], (const char*) key, 0);
-			break;
+			input_path = argv[i + 1];
+			continue;
+		}
+
+		if ((strcmp(argv[i], "--output") == 0) || (strcmp(argv[i], "-o") == 0)) {
+			if (i + 1 >= args) {
+				printf("[?] Missing argument for %s!\n", argv[i]);
+				break;
+			}
+
+			output_path = argv[i+1];
+			continue;
 		}
 	}
 
-	if (!flag) {
-		printf("[?] Invalid argument \'");
-		for (int i = 1; i < args; i++)
-			printf("%s", argv[i]);
-		printf("\'. Try '%s --help' for more info\n", argv[0]);
+	if (!input_path || !output_path) {
+		fprintf(stderr, "[!] Error: Please input a directory and a output directory\n");
 		return 1;
+	}
+
+
+	switch (flag) {
+		case -1:
+			printf("[?] Invalid argument \'");
+			for (int i = 1; i < args; i++)
+				printf("%s", argv[i]);
+			printf("\'. Try '%s --help' for more info\n", argv[0]);
+			break;
+		case 0:
+			// Decrypt
+			iter_folder((const char*) input_path, output_path, key, flag);
+			break;
+		case 1:
+			// Encrypt
+			iter_folder((const char*) input_path, output_path, key, flag);
+			break;
 	}
 
 	return 0;
